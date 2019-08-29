1c1bf2907a51f66498c97afb4f3cbc0f2135591d
GIDEON <9434592+jjdharmaraj@users.noreply.github.com>
2019-08-29 17:52:22 -0500

    Updates

---
+++ precache-manifest.99917f5f37128db37d112fbc15a87f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768c5bb0dd8e9fa900ea702f8e1b095c",
+    "url": "/flash-card-demo/index.html"
+  },
+  {
+    "revision": "59f3a76ccedc57a234c4",
+    "url": "/flash-card-demo/static/css/main.38401444.chunk.css"
+  },
+  {
+    "revision": "9f85af15c9066ebe44a2",
+    "url": "/flash-card-demo/static/js/2.f37aefb3.chunk.js"
+  },
+  {
+    "revision": "59f3a76ccedc57a234c4",
+    "url": "/flash-card-demo/static/js/main.553f325c.chunk.js"
+  },
+  {
+    "revision": "560836e585af96196e82",
+    "url": "/flash-card-demo/static/js/runtime~main.368cee05.js"
+  }
+]);
